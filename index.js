fa3825c61425a09f18af8bbe60316df32c9ab921
papercoffin <ghnguyen03@gmail.com>
2021-05-07 19:48:41 +0000

    ping/pong edit

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ client.on('message', message =>
 {
   if (message.content === '!ping') 
   {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+		// send back "Pong!" to the channel the message was sent in
+		message.channel.send('Pong!');
   }
 });
 
