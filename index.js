9dd00a8fdc5f6010fec96e0eeba565defd35bcd4
papercoffin <ghnguyen03@gmail.com>
2021-05-07 19:47:31 +0000

    ping/pong

--- index.js
+++ index.js
@@ -19,37 +19,14 @@ setInterval(() => {http.get("SITE_URL")}, 280000)
 client.once('ready', () => {console.log('---')})
 
 //Message Event Listener
-client.on('message', message => {
-
-  try {
-
-  //Anything past this line will not happen if the message is not sent in a guild (server)
-  if (!message.guild) return;
-
-  //Anything past this line will not happen if the message is not sent by an user account.
-  if (message.author.bot || message.system) return;
-
-  //Anything past this line will not happen if the message has no prefix.
-  if (!message.content.toLowerCase().startsWith(prefix)) return;
-
-  //Arguments
-  var msgCon = message.content.toLowerCase()
-  var args = message.content.split(' ')
-  var argresult = args.slice(1).join(' ')
-
-  //Message Attachments
-  if (message.attachments.size) {var msgAtt = Array.from(message.attachments.values(), x => x.url)}
-
-  //Classic say command
-  if (msgCon.startsWith(prefix + 'say') && (argresult ||  msgAtt)) {
-    message.channel.send(argresult, {files: msgAtt})
-    message.delete()}
-
-  //Evaluate command
-  if (msgCon.startsWith(prefix + 'eval ') && message.author.id === ownerID) {
-    eval(argresult)}
-
-  } catch(error) {console.log('Trigger: ' + message.content + ' | ' + error)}})
+client.on('message', message => 
+{
+  if (message.content === '!ping') 
+  {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+  }
+});
 
 //Token
 client.login(process.env.TOKEN)
