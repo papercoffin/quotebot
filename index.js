0f6349ad27b2fd924da58c616b9a4046feab6a23
papercoffin <ghnguyen03@gmail.com>
2021-05-07 20:45:01 +0000

    epic build that works (if not then the previous one)

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
-//Variables
+// variables
 var ownerID = "655475175185448985"
 const { prefix } = require('./config.json');
 
-//Packages
+// packages
 const Discord = require('discord.js')
 const client = new Discord.Client();
 const http = require('http')
 const express = require('express')
 const app = express()
 
-//Page
+// page
 var port = (process.env.PORT || 0)
 app.get('/', (req, res) => res.sendStatus(200))
 app.listen(port, () => console.log('Listening at port ' + port))
 setInterval(() => {http.get("https://quotebot1.herokuapp.com/")}, 280000)
 
-//Ready
+// ready
 client.once('ready', () => {console.log('---')})
 
 client.on('message', message => 
 {
-  // Responses using if/else statements.
+  // if/else responses
   
-  // Pings the bot to check for activity.
+  // pings bot to check for activity
   if (message.content === `${prefix}ping`) 
   {
     message.channel.send('Pong!');
   }
 
-  // When a user sends and pastes a link to a Discord message, the bot will display it in an embed.
+  // message quoting
   if (message.content.startsWith("https://discord.com/channels/")) 
   {
     var parts = message.content.split('/'), quoteEmbed = new Discord.MessageEmbed();
-    message.delete();
+
+    message.delete(); // deletes initial message
+
     client.channels.cache.get(parts[5]).messages.fetch(parts[6]).then(nMessage =>
     {
-      quoteEmbed.setColor('#ffd885');
+      // embed settings
+      quoteEmbed.setColor('#fff4d4');
       quoteEmbed.setAuthor(nMessage.author.tag, nMessage.author.displayAvatarURL({ format: 'png', dynamic: true }));
       quoteEmbed.setDescription(nMessage.content + '\n[[Jump to Message]](' + message.content + ')');
       quoteEmbed.setImage((Array.from(nMessage.attachments.values(), x => x.url)[0]));
       quoteEmbed.setFooter(`ID: ${nMessage.id}`);
       quoteEmbed.setTimestamp(nMessage.createdAt);
 
-      // Sends the embed.
+      // sends embed
       message.channel.send(quoteEmbed);
     });
   }
-  
+
 });
 
 //Token
